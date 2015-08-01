5045963800411d2d6acf2fc62e55bea2fe7f4d2e
Jimin Hsieh <jiminhsieh@eztravel.com.tw>
2015-08-01 23:38:31 +0800

    fix groovy script not found

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,21 +1,20 @@
-package Controller
-
 import Config.SetupModule
-import POJO.TestResult
 import Service.SearchService
-import ratpack.jackson.internal.DefaultJsonRender
-
-import static ratpack.jackson.Jackson.json
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import ratpack.jackson.guice.JacksonModule;
 import static ratpack.jackson.Jackson.jsonNode
+import static ratpack.groovy.Groovy.ratpack
 
 /**
  * Created by jimin on 7/28/15.
  */
-import static ratpack.groovy.Groovy.ratpack
+
+final Logger log = LoggerFactory.getLogger(ratpack.class);
 
 ratpack {
     bindings {
+        println("### bindings ###")
         new JacksonModule()
         module SetupModule
     }
@@ -36,7 +35,7 @@ ratpack {
             }
             response.append(" !!!")
 
-            render (response)
+            render(response)
         }
 
         post("foo") {
