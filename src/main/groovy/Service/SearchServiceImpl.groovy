8e76e54729aa0ee587bd789c75c9c9440d69f289
Jimin Hsieh <jiminhsieh@eztravel.com.tw>
2015-08-03 19:26:06 +0800

    Json POJO response & update to ratpack 0.9.19

--- src/main/groovy/Service/SearchServiceImpl.groovy
+++ src/main/groovy/Service/SearchServiceImpl.groovy
@@ -1,6 +1,7 @@
 package Service
 
 import POJO.TestCount
+import POJO.TestResult
 import groovy.transform.CompileStatic
 
 /**
@@ -11,8 +12,10 @@ import groovy.transform.CompileStatic
 class SearchServiceImpl implements SearchService {
 
     @Override
-    boolean search(String keyword) {
+    TestResult search(String keyword) {
         println("### SearchService.search START ###")
+        TestResult result = new TestResult()
+        result.searchWord = keyword
 
         int folderName = TestCount.testCount.addAndGet(1);
         keyword = keyword.replaceAll("\\s+", "+")
@@ -40,9 +43,9 @@ class SearchServiceImpl implements SearchService {
 
         /**
          * TODO: it can be a better way to verify the result, but it maybe need to change source
-         * code of library or use pygoogle
+         * code of robotframework-httplibrary or use pygoogle
          */
-        boolean result = testResult.contains("0 failed") == true ? false : true;
+        result.result = testResult.contains("0 failed") == true ? false : true;
 
         println("### SearchService.search END ###")
         return result
